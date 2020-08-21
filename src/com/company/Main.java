c2ab73165d05450e5c0df906591728953e42bec0
jmaxwell119 <39493833+jmaxwell119@users.noreply.github.com>
2020-08-21 10:12:20 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hi my name is Jason.");
+        System.out.println("I like to make digital art.");
+        System.out.println("I met Jamie Hyneman at NextFest in Chicago when I was a kid.");
+        System.out.println("I helped my neighbor set up a website for her children's books.");
+    }
+}
